c197251333b33465fbcc520f1eef127c678203d5
iamrubenskelvin <rubens.kelvin2015@gmail.com>
2026-06-09 01:18:47 -0300

    RubGera - Sorteio de Numeros (Atividade DevClub)

---
+++ script.js
@@ -0,0 +1,26 @@
+function sortear () {
+
+const numberDe = document.getElementById("entre").value 
+const numberAte = document.getElementById("ate").value 
+
+if (numberDe >= numberAte) {
+    alert("O número inical deve ser menor")
+
+    return
+}
+ 
+if (numberDe < 0 || numberAte <0){
+    alert("Não pode números negativos")
+}
+
+
+const resultado =
+Math.floor(
+    Math.random() * (numberAte - numberDe + 1)
+) + Number(numberDe)
+
+
+document.getElementById("resultado").innerHTML =
+`Número sorteado: ${resultado}`
+
+}
